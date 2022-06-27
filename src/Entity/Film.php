7bb893a86c236ff5a8eb2a5e3655d9926a06961c
Noura-BS <you@example.com>
2022-06-27 21:53:08 +0200

    version1

---
+++ src/Entity/Film.php
@@ -0,0 +1,82 @@
+<?php
+
+namespace App\Entity;
+
+use App\Entity\Film;
+use ApiPlatform\Core\Annotation\ApiResource;
+use App\Repository\FilmRepository;
+use Doctrine\ORM\Mapping as ORM;
+use Symfony\Component\Validator\Constraints as Assert;
+
+/**
+ * @ApiResource()
+ * @ORM\Entity(repositoryClass=FilmRepository::class)
+ */
+class Film
+{
+    /**
+     * @ORM\Id
+     * @ORM\GeneratedValue
+     * @ORM\Column(type="integer")
+     */
+    private $id;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     * @Assert\NotNull(message="Veuillez indiquer le titre")
+     */
+    private $titre;
+
+    /**
+     * @ORM\Column(type="text")
+     * @Assert\NotNull(message="Veuillez indiquer la description")
+     */
+    private $description;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     * @Assert\NotNull(message="Veuillez indiquer le realisateur")
+     */
+    private $realisateur;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getTitre(): ?string
+    {
+        return $this->titre;
+    }
+
+    public function setTitre(string $titre): self
+    {
+        $this->titre = $titre;
+
+        return $this;
+    }
+
+    public function getDescription(): ?string
+    {
+        return $this->description;
+    }
+
+    public function setDescription(string $description): self
+    {
+        $this->description = $description;
+
+        return $this;
+    }
+
+    public function getRealisateur(): ?string
+    {
+        return $this->realisateur;
+    }
+
+    public function setRealisateur(string $realisateur): self
+    {
+        $this->realisateur = $realisateur;
+
+        return $this;
+    }
+}
